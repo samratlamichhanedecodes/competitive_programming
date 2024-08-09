404d8d2040259d62974873346bfcd0af0de79286
Samrat Lamichhane <samratlamichhane708@gmail.com>
2024-08-09 08:09:47 +0545

    codeforces 266A

---
+++ 266A.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	var numberOfStones int
+	var stones string
+
+	fmt.Scan(&numberOfStones)
+	fmt.Scan(&stones)
+
+	var index int
+	var removeCount int
+	for index < numberOfStones-1 {
+		if stones[index] == stones[index+1] {
+			removeCount ++
+			index ++
+		}else{
+			index ++
+		}
+	}
+	fmt.Println(removeCount)
+}
